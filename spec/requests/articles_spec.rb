efea4a1c3db7bd249c1a0607c9d0ce6e19aa985b
sensei100 <tuh42082@temple.edu>
2018-02-26 10:02:37 -0500

    Restrict access for delete action

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -7,6 +7,44 @@ RSpec.describe "Articles", type: :request do
     @article = Article.create!(title: "Title One", body: "Body of Article One", user: @john)
   end
 
+  describe "DELETE/articles/:id" do
+    context "with non-signed in user" do
+      before { delete "/articles/#{@article.id}"}
+
+      it "redirects to the sign in page" do
+        expect(response.status).to eq 302
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+
+    context "with signed-in user who is non-owner" do
+      before do
+        login_as(@fred)
+        delete "/articles/#{@article.id}"
+      end
+
+      it "redirects to the home page" do
+        expect(response.status).to eq 302
+        flash_message = "You can only delete your own article."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+
+    context "successful delete with signed in user as owner" do
+      before do
+        login_as(@john)
+        delete "/articles/#{@article.id}"
+      end
+
+      it "successfully deletes article" do
+        expect(response.status).to eq 302
+        flash_message = "Article has been deleted"
+        expect(flash[:success]).to eq flash_message
+      end
+    end
+  end
+
   describe "GET/articles/:id/edit" do
     context "with non-signed in user" do
       before { get "/articles/#{@article.id}/edit"}
